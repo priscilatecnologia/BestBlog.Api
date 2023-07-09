914566dd554d483bf17ee5d58c54f3a18c3f6d8e
Priscila Silva <priscila.silva@infocar.com.br>
2023-07-09 19:52:43 -0300

    fix: interfaces and abstract class dependency. In addiction add the Injector

---
+++ src/Api/NativeInjectorBootStrapper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Repository;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Application
+
+            // Domain - Events
+
+            // Domain - Commands
+
+            // Infra - Data
+            services.AddScoped<BlogContext>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IPostRepository, PostRepository>();
+
+            // Infra - Data EventSourcing
+        }
+    }
+}
--- src/Repository/PostRepository.cs
+++ src/Repository/PostRepository.cs
@@ -2,7 +2,7 @@ using Model;
 
 namespace Repository
 {
-    public class PostRepository : BaseRepository<Post>
+    public class PostRepository : BaseRepository<Post>, IPostRepository
     {
         public PostRepository(BlogContext context) : base(context) { }
     }
