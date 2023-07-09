ed5f89afc56050f8494c0b6a703c49d0a231734f
Priscila Silva <priscila.silva@infocar.com.br>
2023-07-09 17:22:21 -0300

    feat: add the swaggerUI config

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -3,7 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Repository;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.OpenApi.Models;
 
 namespace Api
 {
@@ -21,11 +25,25 @@ namespace Api
         {
             services.AddDbContext<BlogContext>(x => x.UseInMemoryDatabase("InMemoryDb"));
             services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BestBlog", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
